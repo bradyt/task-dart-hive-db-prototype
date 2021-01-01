449d8c862d5cf19f528315089cb0eb111203dc20
Brady Trainor <git_commit@tangential.info>
2021-01-01 00:00:00 +0000

    Move Storage prototype from test/ to lib/, and refactor input

--- lib/src/task_dart_hive_db_prototype_base.dart
+++ lib/src/task_dart_hive_db_prototype_base.dart
@@ -1,6 +1,107 @@
-// TODO: Put public facing types in this file.
+import 'dart:convert';
+import 'dart:io';
+import 'dart:typed_data';
 
-/// Checks if you are awesome. Spoiler: you are.
-class Awesome {
-  bool get isAwesome => true;
+import 'package:hive/hive.dart';
+
+import 'package:taskc/taskc.dart' as taskc show synchronize;
+import 'package:taskc/taskc.dart' hide synchronize;
+
+class Storage {
+  Storage(this._home);
+
+  final Directory _home;
+  var _box;
+  var _connection;
+  var _credentials;
+
+  Future<void> initializeConfigStorage() async {
+    await Directory('${_home.path}/.task/').delete(recursive: true);
+    await Directory('${_home.path}/.task/').create();
+  }
+
+  Future<void> addTaskrc(File taskrc) => taskrc.copy('${_home.path}/.taskrc');
+
+  Future<void> addPemFiles(Map map) => Future.forEach(map.entries,
+      (entry) => entry.value.copy('${_home.path}/.task/${entry.key}.pem'));
+
+  Future<void> initializeDbAndConnection() async {
+    var taskrc =
+        parseTaskrc(await File('${_home.path}/.taskrc').readAsString());
+
+    var server = taskrc['taskd.server'].split(':');
+    var credentials = Credentials.fromString(taskrc['taskd.credentials']);
+
+    var address = server.first;
+    var port = int.parse(server.last);
+
+    var certificate = '${_home.path}/.task/certificate.pem';
+    var key = '${_home.path}/.task/key.pem';
+    var ca = '${_home.path}/.task/ca.pem';
+
+    _box = await Hive.openBox(
+      'tasks',
+      bytes: Uint8List(0),
+    );
+    _connection = Connection(
+      address: address,
+      port: port,
+      context: SecurityContext()
+        ..useCertificateChain(certificate)
+        ..usePrivateKey(key)
+        ..setTrustedCertificates(ca),
+      onBadCertificate: Platform.isMacOS ? (_) => true : null,
+    );
+    _credentials = credentials;
+  }
+
+  Future<bool> synchronize() async {
+    var response = await taskc.synchronize(
+      connection: _connection,
+      credentials: _credentials,
+      payload: File('${_home.path}/.task/backlog.data').readAsStringSync(),
+    );
+    var header = response.header;
+    var payload = response.payload;
+    switch (header['code']) {
+      case '200':
+        var userKey = payload.userKey;
+        var tasks = payload.tasks.map(
+          (task) => Task.fromJson(json.decode(task)),
+        );
+        tasks.forEach((task) {
+          _box.put(task.uuid, task);
+        });
+        File('${_home.path}/.task/backlog.data').writeAsStringSync(
+          '$userKey\n',
+        );
+        print(header['status']);
+        return true;
+        break;
+      case '201':
+        print(header['status']);
+        return false;
+        break;
+      default:
+        throw Exception(header);
+    }
+  }
+
+  Future<void> addTask(Task task) async {
+    await _box.put(task.uuid, task);
+    await File('${_home.path}/.task/backlog.data').writeAsString(
+      '${json.encode(task.toJson())}\n',
+      mode: FileMode.append,
+    );
+  }
+
+  Future<Map> getTasks() => Future.value(_box.toMap());
 }
+
+// class TaskAdapter extends TypeAdapter<Task> {
+//   var typeId = 0;
+
+//   Task read(BinaryReader reader) {}
+
+//   void write(BinaryWriter writer, Task task) {}
+// }
--- test/task_dart_hive_db_prototype_test.dart
+++ test/task_dart_hive_db_prototype_test.dart
@@ -1,13 +1,9 @@
-import 'dart:convert';
 import 'dart:io';
-import 'dart:typed_data';
 
-import 'package:hive/hive.dart';
 import 'package:test/test.dart';
 import 'package:uuid/uuid.dart';
 
-import 'package:taskc/taskc.dart' as taskc show synchronize;
-import 'package:taskc/taskc.dart' hide synchronize;
+import 'package:taskc/taskc.dart' show Task;
 
 import 'package:task_dart_hive_db_prototype/task_dart_hive_db_prototype.dart';
 
@@ -24,7 +20,18 @@ void main() {
 
     setUp(() async {
       storage = Storage(Directory.systemTemp);
-      await storage.initialize();
+
+      await storage.initializeConfigStorage();
+
+      await storage.addTaskrc(File('fixture/.taskrc'));
+
+      await storage.addPemFiles({
+        'certificate': File('fixture/.task/first_last.cert.pem'),
+        'key': File('fixture/.task/first_last.key.pem'),
+        'ca': File('fixture/.task/ca.cert.pem'),
+      });
+
+      await storage.initializeDbAndConnection();
     });
 
     test('Add a task to hive and read it', () async {
@@ -45,92 +52,3 @@ void main() {
     });
   });
 }
-
-class Storage {
-  Storage(this._home);
-
-  final Directory _home;
-  var _box;
-  var _connection;
-  var _credentials;
-
-  Future<void> initialize() async {
-    var _address = 'localhost';
-    var _port = 53589;
-    var _certificate = 'fixture/.task/first_last.cert.pem';
-    var _key = 'fixture/.task/first_last.key.pem';
-    var _ca = 'fixture/.task/ca.cert.pem';
-
-    await Directory('${_home.path}/.task/').delete(recursive: true);
-    await Directory('${_home.path}/.task/').create();
-
-    _box = await Hive.openBox(
-      'tasks',
-      bytes: Uint8List(0),
-    );
-    _connection = Connection(
-      address: _address,
-      port: _port,
-      context: SecurityContext()
-        ..useCertificateChain(_certificate)
-        ..usePrivateKey(_key)
-        ..setTrustedCertificates(_ca),
-      onBadCertificate: Platform.isMacOS ? (_) => true : null,
-    );
-    _credentials = Credentials.fromString(
-      parseTaskrc(
-        File('fixture/.taskrc').readAsStringSync(),
-      )['taskd.credentials'],
-    );
-  }
-
-  Future<bool> synchronize() async {
-    var response = await taskc.synchronize(
-      connection: _connection,
-      credentials: _credentials,
-      payload: File('${_home.path}/.task/backlog.data').readAsStringSync(),
-    );
-    var header = response.header;
-    var payload = response.payload;
-    switch (header['code']) {
-      case '200':
-        var userKey = payload.userKey;
-        var tasks = payload.tasks.map(
-          (task) => Task.fromJson(json.decode(task)),
-        );
-        tasks.forEach((task) {
-          _box.put(task.uuid, task);
-        });
-        File('${_home.path}/.task/backlog.data').writeAsStringSync(
-          '$userKey\n',
-        );
-        print(header['status']);
-        return true;
-        break;
-      case '201':
-        print(header['status']);
-        return false;
-        break;
-      default:
-        throw Exception(header);
-    }
-  }
-
-  Future<void> addTask(Task task) async {
-    await _box.put(task.uuid, task);
-    await File('${_home.path}/.task/backlog.data').writeAsString(
-      '${json.encode(task.toJson())}\n',
-      mode: FileMode.append,
-    );
-  }
-
-  Future<Map> getTasks() => Future.value(_box.toMap());
-}
-
-// class TaskAdapter extends TypeAdapter<Task> {
-//   var typeId = 0;
-
-//   Task read(BinaryReader reader) {}
-
-//   void write(BinaryWriter writer, Task task) {}
-// }
