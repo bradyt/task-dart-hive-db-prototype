8c8525f82284926a6b029413d3ed68656540454f
Brady Trainor <git_commit@tangential.info>
2021-01-01 00:00:00 +0000

    Seems to be working with just uuid, json pairs

--- test/task_dart_hive_db_prototype_test.dart
+++ test/task_dart_hive_db_prototype_test.dart
@@ -1,16 +1,43 @@
-import 'package:task_dart_hive_db_prototype/task_dart_hive_db_prototype.dart';
+import 'dart:convert';
+import 'dart:typed_data';
+
+import 'package:hive/hive.dart';
 import 'package:test/test.dart';
+import 'package:uuid/uuid.dart';
+
+import 'package:taskc/taskc.dart';
+
+import 'package:task_dart_hive_db_prototype/task_dart_hive_db_prototype.dart';
 
 void main() {
-  group('A group of tests', () {
-    Awesome awesome;
+  group('Test hive usage', () {
+    var box;
 
-    setUp(() {
-      awesome = Awesome();
+    setUp(() async {
+      box = await Hive.openBox(
+        'tasks',
+        bytes: Uint8List(0),
+      );
     });
 
-    test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
+    test('Add a task to hive and read it', () {
+      var task = Task(
+        status: 'pending',
+        uuid: Uuid().v1(),
+        entry: DateTime.now().toUtc(),
+        description: 'hello world',
+      );
+      box.put(task.uuid, task);
+      var stored_task = box.get(task.uuid);
+      print(stored_task.toJson());
     });
   });
 }
+
+// class TaskAdapter extends TypeAdapter<Task> {
+//   var typeId = 0;
+
+//   Task read(BinaryReader reader) {}
+
+//   void write(BinaryWriter writer, Task task) {}
+// }
